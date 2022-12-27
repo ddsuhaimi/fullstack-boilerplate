d0e026f685939a046d1579d46a6c334177f34e08
ddsuhaimi <dedisuhaimiacc@gmail.com>
2022-12-27 21:18:41 +0700

    fixed use app selector

--- packages/web/src/app/dashboard/DashboardLayout.tsx
+++ packages/web/src/app/dashboard/DashboardLayout.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useAppSelector } from "@/redux-toolkit/redux-hooks";
-import { RootState, RootState2, IRootState } from "@/redux-toolkit/store";
+import { RootState } from "@/redux-toolkit/store";
 
 import { useSelector as useReduxSelector, TypedUseSelectorHook } from "react-redux";
 // this directive should be at top of the file, before any imports.
 
 // This is a Client Component
 export default function DashboardLayout({ children }) {
-  const data = useAppSelector((state: RootState) => state);
+  const data = useAppSelector((state: RootState) => state.user);
   return (
     <div>
       <h2>My Dashboard</h2>
